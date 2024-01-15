ca655a1ef17d9b1fab1a276ddb9300c741883236
Vuong Nguyen <nguvuong@sheridancollege.ca>
2024-01-15 11:23:55 -0500

    finish configurating Django admin site

--- catalog/models.py
+++ catalog/models.py
@@ -112,10 +112,18 @@ class Book(models.Model):
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
                                       '">ISBN number</a>')
 
+    language = models.ForeignKey(
+    'Language', on_delete=models.SET_NULL, null=True)
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'   
+
 
     def __str__(self):
         """String for representing the Model object."""
